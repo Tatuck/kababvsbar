5cb852b45d6201932bf66ab6c35864069d4c6b40
Tatuck <tatuck@tatuck.com>
2025-05-18 18:49:43 +0200

    Updated start panel texts

--- src/main/java/com/tatuck/view/StartPanel.java
+++ src/main/java/com/tatuck/view/StartPanel.java
@@ -7,21 +7,29 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.GridBagLayout;
 
 public class StartPanel extends JPanel{
     Font fontButtons = new Font("Arial", Font.BOLD, 40);
     public StartPanel(){
-        this.setLayout(new GridBagLayout());
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.add(Box.createVerticalStrut(250));
 
-        JPanel container = new JPanel();
-        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
-        container.setOpaque(false);
+        JLabel text = new JLabel("El simulador de malasaña más real que existe hasta la época!");
+        text.setAlignmentX(Component.CENTER_ALIGNMENT);
+        text.setFont(new Font("Arial", Font.BOLD, 25));
+        text.setForeground(Color.BLACK);
+        this.add(text);
+        JLabel text1 = new JLabel("La verdadera batalla entre los bares y kebabs!");
+        text1.setAlignmentX(Component.CENTER_ALIGNMENT);
+        text1.setFont(new Font("Arial", Font.BOLD, 25));
+        text1.setForeground(Color.BLACK);
+        this.add(text1);
 
         JButton button1Player = new JButton("1 JUGADOR");
         this.styleButton(button1Player);
@@ -46,11 +54,28 @@ public class StartPanel extends JPanel{
                 cp.getGamePanel().requestFocusInWindow();
             }
         });
+        
+        
 
-        container.add(button1Player);
-        container.add(Box.createVerticalStrut(10));
-        container.add(button2Players);
-        this.add(container);
+        this.add(Box.createVerticalGlue());
+
+        this.add(button1Player);
+        this.add(Box.createVerticalStrut(50));
+        this.add(button2Players);
+
+        this.add(Box.createVerticalGlue());
+
+        JLabel text2 = new JLabel("Movimiento - J1: WASD  | J2: FLECHAS");
+        text2.setAlignmentX(Component.CENTER_ALIGNMENT);
+        text2.setFont(new Font("Arial", Font.BOLD, 15));
+        text2.setForeground(Color.ORANGE);
+        this.add(text2);
+        JLabel text3 = new JLabel("Disparar - J1: ESPACIO  | J2: K");
+        text3.setAlignmentX(Component.CENTER_ALIGNMENT);
+        text3.setFont(new Font("Arial", Font.BOLD, 15));
+        text3.setForeground(Color.ORANGE);
+        this.add(text3);
+        this.add(Box.createVerticalStrut(10));        
     }
 
     private void styleButton(JButton button){
